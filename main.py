8a68edaff37beb7c18e87aaf9a07fff83621633a
Yssssf <mr.yousef.yaghoobi@gmail.com>
2024-09-06 13:05:50 +0100

    venv setted

--- main.py
+++ main.py
@@ -125,6 +125,5 @@ def sortby():
 
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
-    app.run(debug=True)
+    
+    app.run(host="0.0.0.0", port=5000)
